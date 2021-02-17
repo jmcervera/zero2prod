0d80322bd57b6757291aad4c6b100b57ac3bcc96
Juanma <juanma.cervera@cabify.com>
2021-02-17 06:30:00 +0100

    Fix test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,7 @@
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app().await.expect("Failed to spawn our app.");
+    spawn_app();
 
     let client = reqwest::Client::new();
 
@@ -17,6 +17,8 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length())
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-    zero2prod::run().await
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
 }
