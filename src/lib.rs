3715fb2093d554c2d8ce63b6dfa3f166e5a4952e
Juanma <juanma.cervera@cabify.com>
2021-02-18 17:24:36 +0100

    managing configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-mod configuration;
-mod routes;
-mod startup;
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
