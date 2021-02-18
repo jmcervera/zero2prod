3bd4ab590dbd3de68d6a200db13aadbff482cf84
Juanma <juanma.cervera@cabify.com>
2021-02-18 17:35:05 +0100

    Connecting to postgres

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,7 @@
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
+
+use sqlx::postgres::PgColumn;
 use std::net::TcpListener;
 use zero2prod::run;
 
@@ -33,6 +37,12 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
