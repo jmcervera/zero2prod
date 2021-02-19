b247e21a62183c3f6f814d9e0e584d75e9dfa247
Juanma <juanma.cervera@cabify.com>
2021-02-19 07:50:04 +0100

    passing the connection to the workers

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,17 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
+use std::sync::Arc;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = Arc::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .data(connection.clone())
     })
     .listen(listener)?
     .run();
