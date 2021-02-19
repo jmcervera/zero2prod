cf6ea1f6fc801d5392833c0c77b82a23386f770e
Juanma <juanma.cervera@cabify.com>
2021-02-19 12:14:03 +0100

    migrating from log to tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing_futures::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -14,16 +15,16 @@ pub async fn subscribe(
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        email = %form.email,
+        name = %form.name
     );
-    log::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id,
-    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details to the database");
+    tracing::info!("Saving new subscriber details in the database");
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -35,18 +36,11 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     .map_err(|e| {
-        log::error!(
-            "request_id {} - Failed to execute query: {:?}",
-            request_id,
-            e
-        );
+        tracing::error!("Failed to execute query: {:?}", e);
         HttpResponse::InternalServerError().finish()
     })?;
-    log::info!(
-        "request_id {} - New subscriber details have been saved!",
-        request_id
-    );
     Ok(HttpResponse::Ok().finish())
 }
