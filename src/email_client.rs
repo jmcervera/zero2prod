6b3e0e46e3bcf5aaa172ebfd0f4c14a33bd182ed
Juanma <juanma.cervera@cabify.com>
2021-02-21 13:35:08 +0100

    Now the tests are passing

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,30 +1,58 @@
 use crate::domain::SubscriberEmail;
 use reqwest::Client;
 
-#[allow(dead_code)]
 pub struct EmailClient {
     http_client: Client,
     base_url: String,
     sender: SubscriberEmail,
+    authorization_token: String,
 }
 
 impl EmailClient {
-    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+    pub fn new(base_url: String, sender: SubscriberEmail, authorization_token: String) -> Self {
         Self {
             http_client: Client::new(),
             base_url,
             sender,
+            authorization_token,
         }
     }
+
     pub async fn send_email(
         &self,
-        _recipient: SubscriberEmail,
-        _subject: &str,
-        _html_content: &str,
-        _text_content: &str,
-    ) -> Result<(), String> {
-        todo!()
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), reqwest::Error> {
+        let url = format!("{}/email", self.base_url);
+
+        let request_body = SendEmailRequest {
+            from: self.sender.as_ref().to_owned(),
+            to: recipient.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            html_body: html_content.to_owned(),
+            text_body: text_content.to_owned(),
+        };
+
+        self.http_client
+            .post(&url)
+            .header("X-Postmark-Server-Token", &self.authorization_token)
+            .json(&request_body)
+            .send()
+            .await?;
+
+        Ok(())
     }
+}
+
+#[derive(serde::Serialize)]
+struct SendEmailRequest {
+    from: String,
+    to: String,
+    subject: String,
+    html_body: String,
+    text_body: String,
 }
 
 #[cfg(test)]
@@ -42,7 +70,7 @@ mod tests {
         // Arrange
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client = EmailClient::new(mock_server.uri(), sender);
+        let email_client = EmailClient::new(mock_server.uri(), sender, Faker.fake());
 
         Mock::given(any())
             .respond_with(ResponseTemplate::new(200))
