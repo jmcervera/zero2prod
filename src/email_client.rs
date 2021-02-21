9e316fb085ba86d34b11ed15e23e359970db6ea6
Juanma <juanma.cervera@cabify.com>
2021-02-21 12:07:01 +0100

    configuring our email client

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,6 +1,7 @@
 use crate::domain::SubscriberEmail;
 use reqwest::Client;
 
+#[allow(dead_code)]
 pub struct EmailClient {
     http_client: Client,
     base_url: String,
@@ -8,7 +9,7 @@ pub struct EmailClient {
 }
 
 impl EmailClient {
-    pub fn new(base_url: String, sender: SubscriberEmail) -> Seldf {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
         Self {
             http_client: Client::new(),
             base_url,
@@ -17,10 +18,10 @@ impl EmailClient {
     }
     pub async fn send_email(
         &self,
-        recipient: SubscriberEmail,
-        subject: &str,
-        html_content: &str,
-        text_content: &str,
+        _recipient: SubscriberEmail,
+        _subject: &str,
+        _html_content: &str,
+        _text_content: &str,
     ) -> Result<(), String> {
         todo!()
     }
