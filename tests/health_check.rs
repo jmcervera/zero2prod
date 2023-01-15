c2173df6d2831959d60cf3f2135f7395a87aeff6
Juanma <juanmacervera@gmail.com>
2023-01-15 13:17:53 +0100

    first try for the integrations tests. Locks waiting forever

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,22 @@
-use zero2prod::run;
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app().await.expect("Failed to spawn our app.");
 
-#[test]
-fn dummy_test() {
-    run();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> std::io::Result<()> {
+    zero2prod::run().await
 }
