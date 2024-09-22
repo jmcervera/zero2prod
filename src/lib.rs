345aafa12d055328908a7f8d344f348180328d41
Juanma <juanmacervera@gmail.com>
2024-09-22 08:50:08 +0200

    run the project in other port

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ async fn health_check() -> HttpResponse {
 
 pub fn run() -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", 8000))?
         .run();
     Ok(server)
 }
